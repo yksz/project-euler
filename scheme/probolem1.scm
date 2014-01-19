7226fea4ac36b3464c81d7c23417a5123d916359
yksz <user4git@gmail.com>
2014-01-19 12:17:23 +0900

    Add problem1-2 written by scheme

---
+++ scheme/probolem1.scm
@@ -0,0 +1,16 @@
+;; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+;; Find the sum of all the multiples of 3 or 5 below 1000.
+
+(use srfi-1)
+
+(define (sum lis)
+    (fold + 0 lis))
+
+(print
+    (sum
+        (map 
+            (lambda (x)
+                (if (or (= (modulo x 3) 0) (= (modulo x 5) 0))
+                    x
+                    0))
+            (iota 1000))))
